90f42f6d04680d0a9d1dd05486c8dd67d438bf06
Roseskk <oleg419583@yandex.ru>
2022-11-08 17:58:01 +0300

    updated

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import pydrive2
-import traceback
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 from openpyxl import load_workbook
-
-import utils as utl
-
 from pathlib import Path
+
+import traceback
+import utils as utl
 
 
 gauth = GoogleAuth()
@@ -21,44 +19,63 @@ try:
     if Path(path_to_file).exists():
         data = load_workbook(f'{Path(path_to_file)}')
 
-        drive_id = input('Write id of the GoogleDrive -->:')
+        # Смотрим профиль
+        profile = utl.list_folder(drive, '1aNNJp32P5nuVyKq5fG3-2Qmy4Rke-UPJ')
 
-        for name in data.sheetnames:
-            print(f'Group {name}')
+        for name_profile in profile:
+            print(name_profile)
 
-        group = input('Name of group ->: ')
+        title_handler = input('Profile ---->>>>>')
 
-        while group not in data.sheetnames:
-            group = input('Name of group ->: ')
+        # Смотрим курс
+        courses = utl.list_course(title_handler, profile, drive)
 
-        get_list = utl.name_list_data(data[f'{group}'])
-        print(get_list)
-        for name in get_list:
-            metadata = {
-                'parents': [
-                    {"id": f'{drive_id}'}
-                ],
-                'title': f'{name}',
-                'mimeType': 'application/vnd.google-apps.folder'
-            }
-            # Create file
-            file = drive.CreateFile(metadata=metadata)
+        # Выводим курсы
+        for course in courses:
+            print(course)
 
-            file.Upload()
-            # Тут пока что проверка что все работает в поле value почты соответсвенно
-            permission = file.InsertPermission({
-                'type': 'user',
-                'value': '////EMAIL/////',
-                'role': 'reader'})
-            print('.', end='')
+        course_handler = input("Course ------->>>>")
+
+        groups_list = utl.list_group_with_id(course_handler, courses, drive)
+
+        # Тут ID курса смотрим на гугл диске
+        for group in groups_list.items():
+            groups_tuple_course_id = group[-1]
+
+        group_handler_values = dict()
+
+        # Название листов в файле
+        for excel_list_name in data.sheetnames:
+            print('Excel pages(groups): ', excel_list_name)
+
+        group_handler = input("Group ------>>>>")
+
+        while group_handler not in data.sheetnames:
+            group_handler = input("Group ------>>>>")
+
+        # Создаем группу в указаном Курсе
+        utl.create_group_folder(groups_tuple_course_id, group_handler, drive)
+
+        # Получаем ID гугл диска курса
+        updated = utl.list_course(course_handler, courses, drive)
+
+        result_group = str()
+
+        # Берём ID созданной группы
+        for group in updated:
+            if group_handler in group.values():
+                result_group = group.get('id')
+                break
+
+        # Создаем папки студентов и правами
+        utl.create_student_folder(result_group, group_handler, data, drive)
+
     else:
         print('Incorrect path')
 
 
 except Exception as e:
     print('Wrong ID of Google Drive!', traceback.print_exc())
-
-
 
 
 
--- utils.py
+++ utils.py
@@ -1,9 +1,91 @@
 def name_list_data(data):
     name_list = []
-    for i in range(2, data.max_row):
+    for i in range(2, data.max_row+1):
         name = data.cell(row=i, column=2).value
+        email = data.cell(row=i, column=3).value
         if name is None:
             break
         else:
-            name_list.append(name)
+            stud_permission = {'Name': f'{name}', "Email": f'{email}'}
+            name_list.append(stud_permission)
     return name_list
+
+
+def list_folder(drive, parent):
+    filelist = []
+    file_list = drive.ListFile({'q': "'%s' in parents and trashed=false" % parent}).GetList()
+    for f in file_list:
+        if f['mimeType'] == 'application/vnd.google-apps.folder':  # if folder
+            filelist.append({"id": f['id'], "title": f['title'], })
+        else:
+            pass
+    return filelist
+
+
+def list_course(title_handler, profile, drive):
+    title_handler_id = str()
+
+    for name_profile in profile:
+        if title_handler in name_profile.values():
+            title_handler = name_profile
+            title_handler_id = name_profile.get('id')
+
+    course = list_folder(drive, title_handler_id)
+
+    return course
+
+
+def list_group_with_id(title_handler, course, drive):
+    title_handler_id = str()
+
+    for name_group in course:
+        if title_handler in name_group.values():
+            title_handler = name_group
+            title_handler_id = name_group.get('id')
+
+    group = list_folder(drive, title_handler_id)
+
+    return {'group_id': f'{group}', 'title_id': f'{title_handler_id}'}
+
+
+def create_group_folder(course_id, folder_name, drive):
+    metadata = {
+        'parents': [
+            {"id": f'{course_id}'}
+        ],
+        'title': f'{folder_name}',
+        'mimeType': 'application/vnd.google-apps.folder'
+    }
+    # Создание папки
+    file = drive.CreateFile(metadata=metadata)
+    while file.Upload():
+        print('.', end='')
+
+
+def create_student_folder(group_id, folder_name, data, drive):
+    get_list = name_list_data(data[f'{folder_name}'])
+
+    for credentials in get_list:
+
+        name = credentials.get('Name')
+        email = credentials.get('Email')
+
+        metadata = {
+            'parents': [
+                {"id": f'{group_id}'}
+            ],
+            'title': f'{name}',
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+
+        student = drive.CreateFile(metadata=metadata)
+
+        while student.Upload():
+            print('.', end='')
+
+        student.InsertPermission({
+                'type': 'user',
+                'value': f'{email}',
+                'role': 'writer'})
+
+        print('.', end='')
